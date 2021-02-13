2c9db787617748d714519c729575dc8dda662d9b
Alice <70864401+AliceSartori@users.noreply.github.com>
2021-02-13 12:37:15 -0500

    CSVs, Notebook and SQL schema

---
+++ alice/ProjectETL.sql
@@ -0,0 +1,15 @@
+-- CREATE TABLE gross_rent(
+-- 	id INTEGER PRIMARY KEY, 
+-- 	State VARCHAR,
+-- 	State_ab VARCHAR, 
+-- 	City VARCHAR, 
+-- 	Zip_Code INT, 
+-- 	MEAN_Household_Income FLOAT, 
+-- 	STD_Household_Income FLOAT, 
+-- 	MEAN_Gross_Rent INT, 
+-- 	MEDIAN_Gross_Rent INT, 
+-- 	STD_Gross_Rent INT
+-- );
+
+select *
+from gross_rent
